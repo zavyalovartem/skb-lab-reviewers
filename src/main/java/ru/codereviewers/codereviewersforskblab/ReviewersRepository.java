7f057e3c6a3011c27d6dd2967013a3eb66288144
Iulia Chebykina <julia.chebykina.13@gmail.com>
2021-07-04 14:48:51 +0500

    add repo

---
+++ src/main/java/ru/codereviewers/codereviewersforskblab/ReviewersRepository.java
@@ -0,0 +1,6 @@
+package ru.codereviewers.codereviewersforskblab;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ReviewersRepository extends CrudRepository<Reviewers, Long> {
+}
