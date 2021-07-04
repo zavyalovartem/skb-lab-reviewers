1ccc204700a949f43a3f6f90238e89d050c3aac1
Iulia Chebykina <julia.chebykina.13@gmail.com>
2021-07-04 14:34:17 +0500

    'first'

---
+++ src/main/java/ru/codereviewers/codereviewersforskblab/AppointmentReviewersService.java
@@ -0,0 +1,8 @@
+package ru.codereviewers.codereviewersforskblab;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppointmentReviewersService {
+
+}
---
+++ src/main/java/ru/codereviewers/codereviewersforskblab/Reviewers.java
@@ -0,0 +1,27 @@
+package ru.codereviewers.codereviewersforskblab;
+
+import lombok.Data;
+import lombok.NonNull;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class Reviewers {
+    @Id
+    private Long id;
+    @NonNull
+    private Status status;
+
+
+    public Reviewers() {
+
+    }
+}
+enum Status {
+    OVERBURDENED,
+    AVAILABLE,
+    NOT_AVAILABLE,
+    ON_VACATION
+}
