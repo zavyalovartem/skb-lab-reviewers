e3ed53ca884044a45fc41affb5be5d3e949c93f5
Iulia Chebykina <julia.chebykina.13@gmail.com>
2021-07-04 14:55:12 +0500

    a

--- src/main/java/ru/codereviewers/codereviewersforskblab/AppointmentReviewersService.java
+++ src/main/java/ru/codereviewers/codereviewersforskblab/AppointmentReviewersService.java
@@ -1,8 +1,16 @@
 package ru.codereviewers.codereviewersforskblab;
 
+import org.gitlab4j.api.GitLabApi;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AppointmentReviewersService {
+    private final GitLabApi gitLabApi;
+    private static final Logger log = LoggerFactory.getLogger(AppointmentReviewersService.class);
 
+    public AppointmentReviewersService(GitLabApi gitLabApi) {
+        this.gitLabApi = gitLabApi;
+    }
 }
