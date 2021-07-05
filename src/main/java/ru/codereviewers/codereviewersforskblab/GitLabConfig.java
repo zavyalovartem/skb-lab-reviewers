30abd75320a039c6487b3de9f4075a3de32a271c
Iulia Chebykina <julia.chebykina.13@gmail.com>
2021-07-06 00:49:52 +0500

    запускаеися без ошибок!

--- src/main/java/ru/codereviewers/codereviewersforskblab/GitLabConfig.java
+++ src/main/java/ru/codereviewers/codereviewersforskblab/GitLabConfig.java
@@ -3,6 +3,8 @@ package ru.codereviewers.codereviewersforskblab;
 
 import org.gitlab4j.api.GitLabApi;
 import org.gitlab4j.api.GitLabApiException;
+import org.gitlab4j.api.models.Project;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,13 +13,21 @@ import org.springframework.context.annotation.Configuration;
 @ConfigurationProperties(prefix = "app")
 public class GitLabConfig {
 
+    @Value("${app.token}")
     private String token;
+    @Value("${app.host}")
     private String host;
+    @Value("${app.project_id}")
+    private Integer projectId;
 
     @Bean
-    public GitLabApi gitLabApi() throws GitLabApiException {
-        var gitLabApi = new GitLabApi(host, token);
-        gitLabApi.sudo("administrator");
-        return gitLabApi;
+    public GitLabApi gitLabApi(){
+        //        gitLabApi.sudo("administrator");
+        return new GitLabApi(host, token);
+    }
+
+    @Bean
+    public Project project() throws GitLabApiException {
+        return gitLabApi().getProjectApi().getProject(projectId);
     }
 }
